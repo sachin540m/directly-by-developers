df9dec91aef2eafe567697f45db89ee313f032e2
Sachin Maurya <sachinmaurya.work@gmail.com>
2026-09-19 13:20:07 +0530

    Switch to HashRouter for GitHub Pages routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import FloatingButtons from './components/FloatingButtons';
 import AIChatbot from './components/AIChatbot';
